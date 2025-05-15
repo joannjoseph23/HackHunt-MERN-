12047e8c7cfa7d2c32c04f9f5a02f88473c6ab66
joannjoseph23 <joannjoseph23@gmail.com>
2025-05-16 02:03:58 +0530

    Added locations feature for filtering

--- hackathon-backend/hackathons.js
+++ hackathon-backend/hackathons.js
@@ -1,6 +1,6 @@
 const hackathons = [
-  { id: 1, name: 'Hack Delhi', date: '2025-06-10', link: 'https://hackdelhi.in' },
-  { id: 2, name: 'CodeFest IITM', date: '2025-06-18', link: 'https://codefestiitm.in' }
+  { id: 1, name: 'Hack Delhi', date: '2025-06-10', location: 'Delhi', link: 'https://hackdelhi.in' },
+  { id: 2, name: 'CodeFest IIM', date: '2025-06-18', location: 'Chennai', link: 'https://codefestiitm.in' }
 ];
 
 module.exports = hackathons;
