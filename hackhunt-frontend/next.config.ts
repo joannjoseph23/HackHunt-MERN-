ea63a69fe8b65355f16bf09932d59e767e99f6f5
joannjoseph23 <joannjoseph23@gmail.com>
2025-06-05 10:02:52 +0530

    Adding more jobs

--- hackhunt-frontend/next.config.ts
+++ hackhunt-frontend/next.config.ts
@@ -4,12 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['assets.devfolio.co', 'devfolio.co'],
   },
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ already disables ESLint during build
-  },
-  typescript: {
-    ignoreBuildErrors: true, // ✅ add this to skip TS errors during `next build`
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true, // ✅ already disables ESLint during build
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true, // ✅ add this to skip TS errors during `next build`
+  // },
 };
 
 export default nextConfig;
